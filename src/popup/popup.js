0f5a5a5926d23a6495f5e84f91a687bf563bdcf6
Huang, Zeyu <fhfuih@outlook.com>
2023-12-27 21:48:55 +0800

    Add color picker for arbitrary colors

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -36,7 +36,162 @@ function changeColor(course, color) {
     });
 }
 
-// Color variants - grabbed from ust.space source code
+// UI Initialization
+getOrUpdateTimetable(false);
+makeColorCircles();
+const pickr = Pickr.create({
+    el: '.color-picker',
+    // container: '.color-picker-app',
+    theme: 'nano',
+    appClass: 'color-picker-app',
+    inline: true,
+    showAlways: true,
+    useAsButton: true,
+    autoReposition: false,
+    lockOpacity: true,
+    comparison: false,
+    components: {
+        preview: true,
+        opacity: true,
+        hue: true,
+        interaction: {
+            input: true,
+        }
+    },
+    swatches: COLORS,
+})
+pickr
+    .on('change', throttle(handleColorPickerChange, 100))
+    .on('changestop', handleColorPickerChangeStop)
+    .on('swatchselect', handleColorPickerChangeStop)
+    .on('init', () => {
+    const swatchButtons = document.querySelectorAll(".pcr-swatches button");
+    swatchButtons.forEach(function (e, i) {
+        const color = COLORS[i];
+        const backgroundColor = adjustColor(color, "background");
+        const textColor = adjustColor(color, "text");
+        const borderColor = adjustColor(color, "border");
+        e.style.setProperty('--pcr-color', `radial-gradient(circle at center, ${textColor}, ${textColor} 30%, ${backgroundColor} 30%, ${backgroundColor} 80%, ${borderColor} 80%)`);
+    })
+})
+
+// Popup page DOM Manipulations
+document.addEventListener("click", function (e) {
+    if (e.target.matches(".course-item")) {
+        selectCourse(e);
+    } else if (e.target.matches(".option-color-circle")) {
+        selectColorCircle(e);
+    }
+})
+document.getElementById("save-button").addEventListener("click", saveTimetable);
+document.getElementById("reset-button").addEventListener("click", function () {
+    chrome.tabs.reload();
+    window.close()
+});
+document.getElementById("close-button").addEventListener("click", function () {
+    window.close();
+});
+document.getElementById("refresh-button").addEventListener("click", function () {
+    getOrUpdateTimetable(true);
+})
+
+/** @param {HTMLElement} e The course item in the list */
+function selectCourse(e) {
+    selectedCourse = e.target.dataset.course;
+    selectedColor = timetable[selectedCourse];
+    document.querySelectorAll(".course-item").forEach(function (e) {
+        e.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    updateColorCircles();
+    updateColorPicker();
+}
+
+/** @param {HTMLElement} e The color circle in the grid */
+function selectColorCircle(e) {
+    selectedColor = e.target.dataset.color;
+    updateColorCircles();
+    applyChanges();
+}
+
+function handleColorPickerChange(c) {
+    const color = c.toHEXA().toString();
+    const backgroundColor = adjustColor(color, "background");
+    const borderColor = adjustColor(color, "border");
+    const textColor = adjustColor(color, "text");
+    document.querySelector(".timetable-example").style.backgroundColor = backgroundColor;
+    document.querySelector(".timetable-example").style.borderColor = borderColor;
+    document.querySelector(".timetable-example").style.color = textColor;
+}
+
+function handleColorPickerChangeStop(_, instance) {
+    const color = instance.getColor().toHEXA().toString();
+    selectedColor = color;
+    applyChanges();
+}
+
+function applyChanges() {
+    updateCourseDots();
+    changeColor(selectedCourse, selectedColor);
+}
+
+function updateCourseDots() {
+    document.querySelector(".course-item.active .course-color-dot").style.backgroundColor = selectedColor;
+}
+
+function updateColorCircles() {
+    document.querySelectorAll(".option-color-circle").forEach(function (e) {
+        e.classList.remove("active");
+    })
+    document.querySelector(`.option-color-circle[data-color="${selectedColor}"]`).classList.add("active");
+}
+
+function updateColorPicker() {
+    pickr.setColor(selectedColor)
+}
+
+function makeCourses() {
+    const courseContainer = document.getElementById("course-container");
+    courseContainer.innerHTML = "";
+    /** @type HTMLTemplateElement */
+    const courseItemTemplate = document.getElementById("course-item-template");
+    const node = courseItemTemplate.content;
+    for (course in timetable) {
+        const name = course.slice(0, 4) + " " + course.slice(4);
+        const clone = node.cloneNode(true);
+        const courseItem = clone.firstElementChild;
+        courseItem.id = course;
+        courseContainer.append(clone);
+        courseItem.setAttribute("data-course", course);
+        courseItem.querySelector(".course-color-dot").style.backgroundColor = timetable[course];
+        courseItem.querySelector(".course-label").innerText = name;
+    }
+    document.querySelector(".course-item:first-child")?.click();
+}
+
+function makeColorCircles() {
+    const optionContainer = document.getElementById("option-container");
+    /** @type HTMLTemplateElement */
+    const optionItemTemplate = document.getElementById("option-item-template");
+    const node = optionItemTemplate.content;
+    for (let i = 0; i < COLORS.length; i++) {
+        const color = COLORS[i];
+        const clone = node.cloneNode(true);
+        const elem = clone.querySelector(".option-color-circle");
+        elem.setAttribute("data-color", color);
+        elem.style.backgroundColor = adjustColor(color, "background");
+        elem.style.borderColor = adjustColor(color, "border");
+        elem.style.color = adjustColor(color, "text");
+        optionContainer.append(clone);
+    }
+}
+
+// Utilities
+/**
+ * Color variants - grabbed from ust.space source code
+ * @param {string} color The color to adjust
+ * @param {number|'background'|'border'|'text'} adjust The amount to adjust
+ */
 function adjustColor(color, adjust) {
     switch (adjust) {
         case "background":
@@ -59,82 +214,19 @@ function adjustColor(color, adjust) {
     }
 }
 
-// Popup page DOM Manipulations
-document.addEventListener("click", function (e) {
-    if (e.target.matches(".course-item")) {
-        selectedCourse = e.target.dataset.course;
-        selectedColor = timetable[selectedCourse];
-        document.querySelectorAll(".course-item").forEach(function (e) {
-            e.classList.remove("active");
-        })
-        e.target.classList.add("active");
-        updateOptionCircle();
-    } else if (e.target.matches(".option-color-circle")) {
-        selectedColor = e.target.dataset.color;
-        changeColor(selectedCourse, selectedColor);
-        updateCourseDots();
-        updateOptionCircle();
-    }
-})
-document.getElementById("save-button").addEventListener("click", saveTimetable);
-document.getElementById("reset-button").addEventListener("click", function () {
-    chrome.tabs.reload();
-    window.close()
-});
-document.getElementById("close-button").addEventListener("click", function () {
-    window.close();
-});
-document.getElementById("refresh-button").addEventListener("click", function () {
-    getOrUpdateTimetable(true);
-})
-
-function makeCourses() {
-    const courseContainer = document.getElementById("course-container");
-    courseContainer.innerHTML = "";
-    /** @type HTMLTemplateElement */
-    const courseItemTemplate = document.getElementById("course-item-template");
-    const node = courseItemTemplate.content;
-    for (course in timetable) {
-        const name = course.slice(0, 4) + " " + course.slice(4);
-        const clone = node.cloneNode(true);
-        const courseItem = clone.firstElementChild;
-        courseItem.id = course;
-        courseContainer.append(clone);
-        courseItem.setAttribute("data-course", course);
-        courseItem.querySelector(".course-color-dot").style.backgroundColor = timetable[course];
-        courseItem.querySelector(".course-label").innerText = name;
-    }
-    document.querySelector(".course-item:first-child")?.click();
-}
-
-function updateCourseDots() {
-    document.querySelector(".course-item.active .course-color-dot").style.backgroundColor = selectedColor;
-}
-
-function updateOptionCircle() {
-    document.querySelectorAll(".option-color-circle").forEach(function (e) {
-        e.classList.remove("active");
-    })
-    document.querySelector(`.option-color-circle[data-color="${selectedColor}"]`).classList.add("active");
-}
-
-function makeOptions() {
-    const optionContainer = document.getElementById("option-container");
-    /** @type HTMLTemplateElement */
-    const optionItemTemplate = document.getElementById("option-item-template");
-    const node = optionItemTemplate.content;
-    for (let i = 0; i < COLORS.length; i++) {
-        const color = COLORS[i];
-        const clone = node.cloneNode(true);
-        const elem = clone.firstElementChild;
-        elem.setAttribute("data-color", color);
-        elem.style.backgroundColor = adjustColor(color, "background");
-        elem.style.borderColor = adjustColor(color, "border");
-        elem.style.color = adjustColor(color, "text");
-        optionContainer.append(clone);
+/**
+ * @param {Function} func The function to throttle
+ * @param {number} limit The time limit in ms
+ */
+function throttle(func, limit) {
+    let inThrottle
+    return function() {
+        const args = arguments
+        const context = this
+        if (!inThrottle) {
+            func.apply(context, args)
+            inThrottle = true
+            setTimeout(() => inThrottle = false, limit)
+        }
     }
 }
-
-// On load
-getOrUpdateTimetable(false);
-makeOptions();
